2c9fbe781bfb1c24e8bff5bb79eb060205c42c1f
StefanS <vlad.sinescu@ezugi.com>
2023-05-16 09:02:25 +0300

    add server

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/stefan-vl/my-bank/api"
+	db "github.com/stefan-vl/my-bank/db/sqlc"
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
-	fmt.Println("sadas")
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("coul not connect to db", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
+
 }
